2b6e7e06732d6d920e4d28d0b46ee4790736fa3b
Sebastian Wieser <sebastian.wieser2341@gmail.com>
2022-08-30 16:07:52 +0200

    hide secret key

--- VATCheck/settings.py
+++ VATCheck/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-68-c5u8_8tr7_!$0pl!g%_l&v$e^8&*poh9^yde9q!xu3&bcwn"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
