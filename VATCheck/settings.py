fecd224fe255d6dfd79e3ef176f5f08d320b3f40
Sebastian Wieser <sebastian.wieser2341@gmail.com>
2022-09-06 11:45:56 +0200

    update GIT repo

--- VATCheck/settings.py
+++ VATCheck/settings.py
@@ -56,6 +56,15 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
+PASSWORD_HASHERS = [
+    'myproject.hashers.MyPBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
+]
+
 ROOT_URLCONF = "VATCheck.urls"
 
 TEMPLATES = [
