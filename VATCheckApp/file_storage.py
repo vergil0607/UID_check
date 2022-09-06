d55f735cc90bad34c61eb000f346c953d23e013c
Sebastian Wieser <sebastian.wieser2341@gmail.com>
2022-09-06 09:29:01 +0200

    file storage

---
+++ VATCheckApp/file_storage.py
@@ -0,0 +1,24 @@
+from github import Github
+from tempfile import NamedTemporaryFile
+import base64
+import openpyxl
+import os
+
+workbook = 'venv/VATCheck/media/validated_Documents/checked.xlsx'
+
+def store_Github(workbook):
+    g = Github("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")
+
+    f = NamedTemporaryFile(delete=False)
+    workbook.save(f.name)
+    data = f.read()
+    f.close()
+
+    # Then play with your Github objects:
+    repository = g.get_user().get_repo('UID_check')
+    contents = repository.get_contents("UID_numbers.xlsx")
+
+    f = repository.update_file("UID_numbers.xlsx", "Update UID", data, contents.sha)
+
+    return "file was stored"
+
